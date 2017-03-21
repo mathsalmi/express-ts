6648be533a6c369aa6b8d83d2e63da4fbec50c80
Matheus Salmi <mathsalmi@gmail.com>
2017-03-21 13:12:19 -0300

    Add cors configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as express from "express"
 import * as routes from "./routes"
 import * as bodyparser from "body-parser"
+import * as cors from "cors"
 
 // constants
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,8 @@ const app: express.Express = express();
 app.use(bodyparser.raw());
 app.use(bodyparser.text());
 app.use(bodyparser.json());
+
+app.use(cors());
 
 // routes
 routes.set(app);
