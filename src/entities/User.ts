eb9ead4f6ada50321243542d8e9b5e3f0398d690
Matheus Salmi <mathsalmi@gmail.com>
2017-03-21 13:05:00 -0300

    Fix User entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,33 @@
-export class User {
-	public id: number
-	public name: string
+import * as sequelize from "sequelize"
+import * as connect from "../conf/connection"
+
+export interface UserAttribute {
+	id?: string;
+	name?: string;
 }
+
+export interface UserInstance extends sequelize.Instance<UserAttribute>, UserAttribute {
+}
+
+export interface UserModel extends sequelize.Model<UserInstance, UserAttribute> {
+}
+
+export const User = connect.define<UserInstance, UserAttribute>('user', {
+	id: {
+		type: sequelize.INTEGER,
+		primaryKey: true
+	},
+	name: {
+		type: sequelize.STRING,
+		validate: {
+			obrigarNomeSalmi: (value: any) => {
+				if (value != 'matheus') {
+					throw Error("Must be user matheus")
+				}
+			}
+		}
+	},
+}, {
+		tableName: 'user',
+		timestamps: false
+	});
